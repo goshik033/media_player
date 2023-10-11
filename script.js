466dfaf23dae4e02c5f291bce1b0bafcc4b03416
goshik033 <sk.gosha@yandex.ru>
2023-10-11 21:53:33 +0300

    Добавил звукоанализатор

--- script.js
+++ script.js
@@ -70,22 +70,6 @@ function prevSong() {
 
 prevBtn.addEventListener('click', prevSong);
 
-// function updateProgress(e) {
-//     const {duration, currentTime} = e.srcElement;
-//     const progressPercent = currentTime / duration * 100
-//     progress.style.width = `${progressPercent}%`;
-// }
-// audio.addEventListener('timeupdate', updateProgress)
-
-// function setProgress(e) {
-//     const width = this.clientWidth;
-//     const clickX = e.offsetX
-//     const duration = audio.duration
-//     audio.currentTime = clickX / width * duration;
-// }
-// progressContainer.addEventListener('click', setProgress)
-
-
 audio.addEventListener('timeupdate', () => {
     const currentTime = audio.currentTime;
     const duration = audio.duration;
@@ -108,3 +92,99 @@ progressSlider.addEventListener('input', () => {
 });
 
 audio.addEventListener('ended', nextSong)
+
+
+
+// ---------------------------- ЭКВАЛАЙЗЕР ----------------------------
+const columnsGap = 2;
+const columnCount = 256; // Кол-во колонок: 1024, 512, 256, 128, 64
+
+const canvas = document.getElementById('player-fireplace');
+const ctx = canvas.getContext('2d');
+
+let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+let source = audioCtx.createMediaElementSource(audio);
+let analyser = audioCtx.createAnalyser();
+source.connect(analyser); // Подключаем анализатор к элементу audio
+analyser.connect(audioCtx.destination);
+let frequencyData = new Uint8Array(columnCount);
+
+window.addEventListener('resize', resizeCanvas, false);
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resizeCanvas();
+
+// Рисует колонку вертикально
+function drawColumnRight(y, height, width) {
+    const cornerRadius = 5; // Радиус закругления углов
+    const gradient = ctx.createLinearGradient( canvas.width - width / 2, y, canvas.width,y);
+    gradient.addColorStop(0, "rgba(255,0,0,0)");
+    // gradient.addColorStop(1, "rgba(255,255,255,1)");
+    gradient.addColorStop(0.9, "#ff0077");
+
+    ctx.fillStyle = gradient;
+
+
+
+    // Установка градиента в качестве заливки
+    ctx.fillStyle = gradient;
+
+    // Рисование прямоугольника с закругленными углами
+    ctx.beginPath();
+    ctx.moveTo(canvas.width - width / 2, y + cornerRadius);
+    ctx.arcTo(canvas.width - width / 2, y, canvas.width, y, cornerRadius);
+    ctx.arcTo(canvas.width, y, canvas.width, y + height, 0);
+    ctx.arcTo(canvas.width, y + height, canvas.width - width / 2, y + height, 0);
+    ctx.arcTo(canvas.width - width / 2, y + height, canvas.width - width / 2, y, cornerRadius);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function drawColumn(y, height, width) {
+    const cornerRadius = 5; // Радиус закругления углов
+
+    const gradient = ctx.createLinearGradient( width / 2, y, 0,y);
+    gradient.addColorStop(0, "rgba(255,0,0,0)");
+    gradient.addColorStop(0.9, "red");
+
+    ctx.fillStyle = gradient; // Лаймовый цвет
+
+    // Рисование прямоугольника с закругленными углами
+    ctx.beginPath();
+    ctx.moveTo( width / 2, y + cornerRadius);
+    ctx.arcTo(width / 2, y, 0, y, cornerRadius);
+    ctx.arcTo(0, y, 0, y + height, 0);
+    ctx.arcTo(0, y + height, width / 2, y + height, 0);
+    ctx.arcTo( width / 2, y + height,  width / 2, y, cornerRadius);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function render() {
+    analyser.getByteFrequencyData(frequencyData); // Записываем в массив данные уровней частот
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const columnWidth = 12;
+    const heightScale = canvas.width / 300; // Масштабный коэффициент
+
+    let yPos = 0;
+    let xPos = 0;
+
+    for (let i = 0; i < frequencyData.length; i++) {
+        let columnHeight = frequencyData[i] * heightScale;
+
+        // Рисуем колонки справа
+        drawColumnRight(yPos, columnWidth, columnHeight);
+        yPos += columnWidth + columnsGap;
+
+        drawColumn(xPos, columnWidth, columnHeight);
+        xPos += columnWidth + columnsGap;
+    }
+
+    window.requestAnimationFrame(render);
+}
+
+window.requestAnimationFrame(render);
